acbc68821482bab7a9989baa8a5f3b1a87afc13d
BranikMacik <75607772+BranikMacik@users.noreply.github.com>
2023-04-19 17:07:16 +0200

    Add files via upload

---
+++ benchmarkingsql/src/main/java/com/macko/services/DatabaseEnums.java
@@ -0,0 +1,23 @@
+package com.macko.services;
+/*
+ * This calss stores the credentials needed to access the database server. By keeping the credentials in a single class, 
+ * we can easily update them if they change and ensure that they are used consistently throughout the project. Additionally, 
+ * by using an enum to represent the credentials, we can ensure that they are only used in the intended way, and prevent accidental
+ * misuse or modification of the credentials. This class should be used by other classes that need to access the database, 
+ * but should not be modified directly at runtime.  
+ */
+
+public class DatabaseEnums {
+    //Database URL used for database connectivity
+    public static final String DB_URL = "";
+
+    //Enums representing the tables present in the database
+    public enum Tables {
+
+    }
+
+    //Enums of the credentials for accessing the database
+    public enum Credentials {
+
+    }
+}
